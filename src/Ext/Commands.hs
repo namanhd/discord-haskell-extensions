906f53ee66c0d53bf2002ee4e23cbafe3977848e
Nam Anh Dinh <namanh@uchicago.edu>
2020-06-21 20:20:33 -0500

    Broke up parseAndEvalCommand into smaller funcs

--- src/Ext/Commands.hs
+++ src/Ext/Commands.hs
@@ -125,28 +125,46 @@ restCreateMessage (dis, msg) t =
 
 -- | =Evaluating commands= ---------------------
 
--- | Parse and try to evaluate the command into an IO action
+-- | Get prefix text of command. This is whatever the inputted command name is 
+-- (right after the command prefix, but before the first space)
+getPrefixText :: T.Text -> T.Text
+getPrefixText = T.takeWhile (not . isSpace)
+
+-- | Find what the main name of a command is from its alias if possible
+resolveCommandAlias :: AliasMap -> T.Text -> Either String T.Text
+resolveCommandAlias aliasMap remains = case M.lookup (getPrefixText remains) aliasMap of
+  Nothing       -> Left "[resolveCommandAlias] Input is not an alias of any recognized command"
+  Just mainName -> Right mainName
+
+-- | Get the Command with the resolved name
+getCommandFromResolvedName :: CommandTree -> T.Text -> Either String Command
+getCommandFromResolvedName [] _ = Left "[getCommandFromResolvedName] No commands were defined"
+getCommandFromResolvedName (cmd:cmds) mainName = case mainName == name cmd of
+  False -> getCommandFromResolvedName cmds mainName
+  True  -> Right $ command cmd
+
+-- | Evaluate the found command based on what kind it is
+evalCommand :: Context -> AliasMap -> T.Text -> Command -> Either String (IO ())
+evalCommand ctx aliasMap remains cmd = case cmd of 
+  CmdN _ -> Right $ runCmdIOWith suffixText
+  CmdA _ -> Right $ runCmdIOWith suffixText
+  Cmd0 _ -> Right $ runCmdIOWith (T.empty)
+  Cmds _ subCmds -> case T.strip suffixText == T.empty of
+    True  -> Right $ runCmdIOWith (T.empty)
+    False -> case parseAndEvalCommand ctx subCmds aliasMap suffixText of
+      Left errorMsg   -> Right $ runCmdIOWith (T.empty)
+      Right ioResult  -> Right ioResult
+  where
+    suffixText = T.drop (1 + T.length (getPrefixText remains)) remains
+    runCmdIOWith = runCmdIO ctx cmd
+
+-- | Sequences together above components to parse and evaluate a command string
+-- into Either a wrapped IO action or a wrapped error message
 parseAndEvalCommand :: Context -> CommandTree -> AliasMap -> T.Text -> Either String (IO ())
-parseAndEvalCommand ctx [] _ remains = 
-  Left $ "No command was defined"
-parseAndEvalCommand ctx (cmd:cmds) aliases remains =
-  case M.lookup prefixText aliases of
-    Nothing -> Left $ "Command not found; parsed up to '" ++ T.unpack remains ++ "' before failing"
-    Just mainName -> case mainName == name cmd of
-      False -> parseAndEvalCommand ctx cmds aliases remains
-      True -> case command cmd of
-        CmdN _ -> Right $ runCmdIOWith suffixText
-        CmdA _ -> Right $ runCmdIOWith suffixText
-        Cmd0 _ -> Right $ runCmdIOWith (T.empty)
-        Cmds _ subCmds -> case T.strip suffixText == T.empty of
-          True  -> Right $ runCmdIOWith (T.empty)
-          False -> case parseAndEvalCommand ctx subCmds aliases suffixText of
-            Left errorMsg   -> Right $ runCmdIOWith (T.empty)
-            Right ioResult  -> Right ioResult
-    where 
-  prefixText = T.takeWhile (not . isSpace) remains
-  suffixText = T.drop (1 + T.length prefixText) remains
-  runCmdIOWith = runCmdIO ctx (command cmd)
+parseAndEvalCommand ctx cmdTree aliasMap remains = 
+  resolveCommandAlias aliasMap remains 
+  >>= getCommandFromResolvedName cmdTree 
+  >>= evalCommand ctx aliasMap remains
 
 -- | Do the user-defined IO () function for the command
 runCmdIO :: Context -> Command -> T.Text -> IO ()
@@ -155,44 +173,44 @@ runCmdIO ctx (Cmd0 func) _ = func ctx
 runCmdIO ctx (CmdA func) argtext = func ctx argtext
 runCmdIO ctx (CmdN multiArgFunc) argtext = 
   runMultiArgIO ctx multiArgFunc $ splitPreserveQuotes argtext
-    where  
-  splitOnlyOutOfQuotes :: Int -> [T.Text] -> [[T.Text]]
-  splitOnlyOutOfQuotes _ [] = []
-  splitOnlyOutOfQuotes i (sub:subs) =
-    -- Even-indexed items in the output list are outside of quotes. Odds are inside. 
-    (if (i `rem` 2 == 0) then T.split isSpace sub else [sub]) : splitOnlyOutOfQuotes (i+1) subs
+  where
+    splitOnlyOutOfQuotes :: Int -> [T.Text] -> [[T.Text]]
+    splitOnlyOutOfQuotes _ [] = []
+    splitOnlyOutOfQuotes i (sub:subs) =
+      -- Even-indexed items in the output list are outside of quotes. Odds are inside. 
+      (if (i `rem` 2 == 0) then T.split isSpace sub else [sub]) : splitOnlyOutOfQuotes (i+1) subs
 
-  splitPreserveQuotes :: T.Text -> [T.Text]
-  splitPreserveQuotes = 
-    filter (not . T.null) . join . splitOnlyOutOfQuotes 0 . T.split (=='"') 
+    splitPreserveQuotes :: T.Text -> [T.Text]
+    splitPreserveQuotes = 
+      filter (not . T.null) . join . splitOnlyOutOfQuotes 0 . T.split (=='"') 
 
 -- | Do the user-defined multi-argument IO () function for the command
 runMultiArgIO :: Context -> MultiArgCommandFunction -> [T.Text] -> IO ()
 runMultiArgIO ctx@(dis,msg) multiArgFunc args = 
   runMaybeT (runMultiArgIOMaybe ctx multiArgFunc args) *> pure ()
-    where
-  runMultiArgIOMaybe :: Context -> MultiArgCommandFunction -> [T.Text] -> MaybeT IO ()
-  runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc1 func) [a] = do
-    u <- fromMention dis a
-    lift $ func ctx u 
-  runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc2 func) [a,b] = do
-    -- We can't pack and fmap them together because they have different types
-    u <- fromMention dis a
-    v <- fromMention dis b
-    lift $ func ctx u v
-  runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc3 func) [a,b,c] = do
-    u <- fromMention dis a
-    v <- fromMention dis b
-    w <- fromMention dis c
-    lift $ func ctx u v w
-  runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc4 func) [a,b,c,d] = do
-    u <- fromMention dis a 
-    v <- fromMention dis b
-    w <- fromMention dis c
-    x <- fromMention dis d
-    lift $ func ctx u v w x
-  runMultiArgIOMaybe _ _ _ = 
-    lift $ putStrLn "[runMultiArgIO] Error in pattern match: Invalid number of arguments"
+  where
+    runMultiArgIOMaybe :: Context -> MultiArgCommandFunction -> [T.Text] -> MaybeT IO ()
+    runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc1 func) [a] = do
+      u <- fromMention dis a
+      lift $ func ctx u 
+    runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc2 func) [a,b] = do
+      -- We can't pack and fmap them together because they have different types
+      u <- fromMention dis a
+      v <- fromMention dis b
+      lift $ func ctx u v
+    runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc3 func) [a,b,c] = do
+      u <- fromMention dis a
+      v <- fromMention dis b
+      w <- fromMention dis c
+      lift $ func ctx u v w
+    runMultiArgIOMaybe ctx@(dis, msg) (CmdFunc4 func) [a,b,c,d] = do
+      u <- fromMention dis a 
+      v <- fromMention dis b
+      w <- fromMention dis c
+      x <- fromMention dis d
+      lift $ func ctx u v w x
+    runMultiArgIOMaybe _ _ _ = 
+      lift $ putStrLn "[runMultiArgIO] Error in pattern match: Invalid number of arguments"
 
 -- | Unpack and execute the IO action eval'd from the command, or report if invalid command
 executeEvaluatedCommand :: Either String (IO ()) -> IO ()
